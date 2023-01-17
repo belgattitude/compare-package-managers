fe1abec891c5775b2625c8bcbfbda08ef937764f
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-01-17 21:21:07 +0100

    chore: pnpm 7.25.0 / yarn 4.0.0-rc.35

--- create-benchmark-lock-files.sh
+++ create-benchmark-lock-files.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
+npx rimraf@3 '*.lock'
+
 rm -f '.yarn/install-state.gz'
-npx rimraf '**/node_modules'
+npx rimraf@3 '**/node_modules'
 
 YARN_COMPRESSION_LEVEL=mixed YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock yarn install
 YARN_COMPRESSION_LEVEL=mixed YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock yarn dedupe
 
 rm -f '.yarn/install-state.gz'
-npx rimraf '**/node_modules'
+npx rimraf@3 '**/node_modules'
 
 YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock yarn install
 YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock yarn dedupe
 
 rm -f '.yarn/install-state.gz'
-npx rimraf '**/node_modules'
+npx rimraf@3 '**/node_modules'
 
 YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn install
 YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn dedupe
 
-npx rimraf '**/node_modules'
+npx rimraf@3 '**/node_modules'
 
 rm ./pnpm-lock.yaml
 

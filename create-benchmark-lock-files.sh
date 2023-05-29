198067d23195809e8e4034e79a03daebce6f119a
Sébastien Vanvelthem <belgattitude@users.noreply.github.com>
2023-05-29 11:04:12 +0200

    feat: add pnp linker (#17)
    * feat: add pnp linker
    
    * feat: add pnp linker
    
    * feat: add pnp linker
    
    * feat: add pnp linker
    
    * feat: add pnp linker
    
    * feat: add pnp linker

--- create-benchmark-lock-files.sh
+++ create-benchmark-lock-files.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-rm -f '.yarn/install-state.gz'
-npx -y rimraf@latest --glob '**/node_modules'
+npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-state.gz'
 
 YARN_COMPRESSION_LEVEL=mixed YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock yarn install
 YARN_COMPRESSION_LEVEL=mixed YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock yarn dedupe
 
-rm -f '.yarn/install-state.gz'
-npx -y rimraf@latest --glob '**/node_modules'
+npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-state.gz'
 
 YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock yarn install
 YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock yarn dedupe
 
-#rm -f '.yarn/install-state.gz'
-#npx -y rimraf@3.0.1 '**/node_modules'
+npx -y rimraf@5.0.1 '**/node_modules' '.yarn/install-state.gz'
 
-#YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn install
-#YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn dedupe
+YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn install
+YARN_NODE_LINKER=pnp YARN_COMPRESSION_LEVEL=0 YARN_LOCKFILE_FILENAME=yarn.pnp.lock yarn dedupe
 
-npx -y rimraf@latest --glob '**/node_modules'
+npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-state.gz'
 
 rm ./pnpm-lock.yaml
 

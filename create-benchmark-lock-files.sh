e3d6eca95d56946cd7def6ca1a751c83453ad8df
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-06-16 17:33:37 +0200

    feat: update with upstream nextjs-monorepo-example

--- create-benchmark-lock-files.sh
+++ create-benchmark-lock-files.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 
-YARN_VERSION="4.0.0-rc.45"
+BENCH_YARN_VERSION=4.0.0-rc.45
+BENCH_PNPM_VERSION=8.6.2
 
 export PRISMA_SKIP_POSTINSTALL_GENERATE=true PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=true HUSKY=0
 
 npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
-YARN_RC_FILENAME=.yarnrc.mixed-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
-YARN_RC_FILENAME=.yarnrc.mixed-compress.yml corepack yarn@${YARN_VERSION} dedupe
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock YARN_RC_FILENAME=.yarnrc.mixed-compress.yml YARN_COMPRESSION_LEVEL=mixed YARN_CACHE_FOLDER=.yarn/benchmark-cache/mixed-compress corepack yarn@${BENCH_YARN_VERSION} install --inline-builds
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.mixed-compress.lock YARN_RC_FILENAME=.yarnrc.mixed-compress.yml YARN_COMPRESSION_LEVEL=mixed YARN_CACHE_FOLDER=.yarn/benchmark-cache/mixed-compress corepack yarn@${BENCH_YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
 
-YARN_RC_FILENAME=.yarnrc.no-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
-YARN_RC_FILENAME=.yarnrc.no-compress.yml corepack yarn@${YARN_VERSION} dedupe
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock YARN_RC_FILENAME=.yarnrc.no-compress.yml YARN_COMPRESSION_LEVEL=0 YARN_CACHE_FOLDER=.yarn/benchmark-cache/no-compress corepack yarn@${BENCH_YARN_VERSION} install --inline-builds
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.no-compress.lock YARN_RC_FILENAME=.yarnrc.no-compress.yml YARN_COMPRESSION_LEVEL=0 YARN_CACHE_FOLDER=.yarn/benchmark-cache/no-compress corepack yarn@${BENCH_YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
 
-YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
-YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml corepack yarn@${YARN_VERSION} dedupe
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.pnp.no-compress.lock YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml YARN_CACHE_FOLDER=.yarn/benchmark-cache/pnp-no-compress corepack yarn@${BENCH_YARN_VERSION} install --inline-builds
+COREPACK_ENABLE_STRICT=0 YARN_LOCKFILE_FILENAME=yarn.pnp.no-compress.lock YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml YARN_CACHE_FOLDER=.yarn/benchmark-cache/pnp-no-compress corepack yarn@${BENCH_YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
 
 rm ./pnpm-lock.yaml
 
-pnpm i
+COREPACK_ENABLE_STRICT=0 corepack pnpm@${BENCH_PNPM_VERSION} i
 
-pnpm dedupe
+COREPACK_ENABLE_STRICT=0 corepack pnpm@${BENCH_PNPM_VERSION} dedupe

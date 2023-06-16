89c7c25438ea3fc4c4225d02ff1e4d8a152cd07e
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-06-16 16:34:51 +0200

    chore: pnpm 8.6.2

--- create-benchmark-lock-files.sh
+++ create-benchmark-lock-files.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 
+YARN_VERSION="4.0.0-rc.45"
+
 export PRISMA_SKIP_POSTINSTALL_GENERATE=true PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=true HUSKY=0
 
+npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
+YARN_RC_FILENAME=.yarnrc.mixed-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
+YARN_RC_FILENAME=.yarnrc.mixed-compress.yml corepack yarn@${YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
-YARN_RC_FILENAME=.yarnrc.mixed-compress.yml yarn install --inline-builds
-YARN_RC_FILENAME=.yarnrc.mixed-compress.yml yarn dedupe
 
-npx -y rimraf@5.0.1 --glob '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
-
-YARN_RC_FILENAME=.yarnrc.no-compress.yml yarn install --inline-builds
-YARN_RC_FILENAME=.yarnrc.no-compress.yml yarn dedupe
+YARN_RC_FILENAME=.yarnrc.no-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
+YARN_RC_FILENAME=.yarnrc.no-compress.yml corepack yarn@${YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
 
-YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml yarn install --inline-builds
-YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml yarn dedupe
+YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml corepack yarn@${YARN_VERSION} install --inline-builds
+YARN_RC_FILENAME=.yarnrc.pnp.no-compress.yml corepack yarn@${YARN_VERSION} dedupe
 
 npx -y rimraf@5.0.1 '**/node_modules' '.yarn/install-stat*.gz' '.yarn/unplugged'
 
